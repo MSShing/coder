f4bf3ebadb0428e827b6e449211ebc383c3c2825
MSShing <mayshing857@gmail.com>
2024-08-17 11:15:46 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+// "use strict"; treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs not browser 
+
+let name = "mayur"
+let age = 19
+let isLoggedIn = false
+
+// number range => 2^53
+// bigint
+// string => use in ""
+// boolean => true/false
+// mull =>Standalone value 
+// undefined
+// symbol => unique
+
+// object
+console.log(typeof null); 
+console.log(typeof undefined)
+console.log(typeof "Mayur");
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 123456
+let accountEmail = "mayur@google.com"
+var accountPassword = "7890"
+accountCity = "Porbandar"
+let accountState;
+
+// but accountId = 2 not allowed
+
+accountEmail = "ms@gmail.com"
+accountPassword = "121212"
+accountCity = "Rajkot"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
